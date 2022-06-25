e603e0e67e3614aa39f3c8c3ed7371c62af84f90
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-06-25 23:27:15 +0200

    login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -18,7 +18,30 @@ router.post('/register', async (req,res) =>{
         res.status(500).json(err)
 
     }
+
+    //login
+    router.post('/login', async (req,res) =>{
+        try{
+            const user =  await User.findOne({email:req.body.email})
+            !user &&res.status(401).json('wrong password or username')
+
+            const bytes=CryptoJS.AES.decrypt(user.password,process.env.SECRET_KEY)
+            const originalPassword = bytes.toString(CryptoJS.enc.Utf8)
+
+            originalPassword !== req.body.password && res.status(401).json('wrong password or username')
+
+            const { password ,...info } = user._doc
+            res.status(200).json(user)
+        }
+        catch(err){
+            res.status(500).json(err)
+
+        }
+
+    }
+
+
    
 
 })
-module.exports = router;
+module.exports  = router;
